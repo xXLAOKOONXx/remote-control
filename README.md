3efc88163b4b7749d61541235f6d2dc2d51ade70
unknown <tom.eckstein@web.de>
2024-03-05 23:07:09 +0100

    INIT

---
+++ README.md
@@ -0,0 +1,42 @@
+# Remote Controller
+
+This is web app to start files from a different device.
+
+## Usage
+
+### Guide
+
+On app start you are required to give two information.
+
+1. A file dialog opens to select the folder with the files
+2. A console window opens asking for the file ending to search for
+
+The webapp is available at `localhost:80` and via ip adress within your local network.
+
+The ip adresses are shown in the console as well.
+
+To close the webapp press Ctrl + C within the Console or close the console window.
+
+For each startable file with the correct file ending there will be a button with the filename as display text.
+
+### Use code
+
+Install poetry package
+
+```bash
+pip install poetry
+```
+
+Install poetry environment
+
+```bash
+poetry install
+```
+
+Start the app
+
+```bash
+poetry run python remote-control/app.py
+```
+
+### Use exe
---
+++ remote-control/app.py
@@ -0,0 +1,57 @@
+import os
+from pathlib import Path
+from dash import Dash, html, dcc, Input, Output, State, MATCH
+import dash_bootstrap_components as dbc
+import dash
+from tkinter import filedialog
+
+# Select folder
+FOLDER = Path(filedialog.askdirectory())
+
+# Select file type
+FILE_TYPE = 'fpl'
+FILE_TYPE = input('Enter file type (defaults to "fpl"): ') or FILE_TYPE
+
+app = Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP],
+              suppress_callback_exceptions=True)
+
+def get_playlist_store_id(playlist:str):
+    return {'type': 'playlist-data', 'playlist': playlist}
+
+def get_playlist_button_id(playlist:str):
+    return {'type': 'playlist-button', 'playlist': playlist}
+
+def build_playlist_button(display_name:str, full_path:str):
+    return html.Div([
+        dbc.Button(display_name, id=get_playlist_button_id(display_name), n_clicks=0),
+        dcc.Store(id=get_playlist_store_id(display_name), data=full_path)    
+    ])
+
+@app.callback(
+    Output(get_playlist_button_id(MATCH), 'style'),
+    [Input(get_playlist_button_id(MATCH), 'n_clicks'),
+     State(get_playlist_store_id(MATCH), 'data')]
+)
+def start_file(n_clicks, playlist):
+    if n_clicks:
+        os.startfile(playlist)
+    return dash.no_update
+
+playlists = []
+for file in FOLDER.glob(f'*.{FILE_TYPE}'):
+    display_name = file.stem.strip()
+    playlists.append(build_playlist_button(display_name, str(file)))
+
+app.title = 'Remote-Control'
+
+app.layout = html.Div(id='full-body',
+    children=[
+        html.Div(id='playlist-buttons',
+            children=
+        playlists),
+], 
+)
+
+
+if __name__ == '__main__':
+    app.run_server(debug=False, port=80, host='0.0.0.0')
